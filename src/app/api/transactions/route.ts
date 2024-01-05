96dcf1601a6462335591201ceb4bf14190fae37f
Felipe Borges <felipejsborges@outlook.com>
2024-01-05 18:09:00 -0300

    feat: allow to filter and sort transactions (back-end)

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -1,9 +1,51 @@
-import { PrismaClient } from '@prisma/client'
-import { NextResponse } from 'next/server'
+import { Prisma, PrismaClient } from '@prisma/client'
+import { NextRequest, NextResponse } from 'next/server'
 
 const prisma = new PrismaClient()
 
-export async function GET() {
-	const transactions = await prisma.transaction.findMany()
+function transformNextUrlSearchParamsInObject(
+	nextUrlSearchParams: URLSearchParams
+) {
+	const result: any = {}
+	for (const [key, value] of nextUrlSearchParams.entries()) {
+		result[key] = value
+	}
+	return result
+}
+
+export async function GET(request: NextRequest) {
+
+	const { search, sort, ...filters } = transformNextUrlSearchParamsInObject(request.nextUrl.searchParams)
+
+	let where: Prisma.TransactionWhereInput = {}
+	if (search) {
+		where.description = { contains: search }
+	}
+	if (filters.tags) {
+		where.tags = { some: { name: filters.tags as string } }
+	}
+	where = {
+		...where,
+		...filters,
+	}
+
+	const orderBy: Prisma.TransactionOrderByWithAggregationInput[] = []
+	if (sort) {
+		const sortItems = sort.split(',')
+		sortItems.forEach((sortItem: any) => {
+			let order: Prisma.SortOrder = 'asc'
+			if (sortItem.startsWith('-')) {
+				order = 'desc'
+				sortItem = sortItem.substring(1) // Remove the leading "-"
+			}
+			orderBy.push({ [sortItem]: order })
+		})
+	}
+
+	const transactions = await prisma.transaction.findMany({
+		where,
+		orderBy,
+	})
+
 	return NextResponse.json(transactions)
 }
---
+++ todo.md
@@ -0,0 +1,37 @@
+Uso pessoal
+	- [X] Adicionar campos nas transactions:
+		- [X] forma (débito, boleto, pix, crédito)
+		- [X] destino
+		- [X] id: slug do user_id, data, descricao, valor
+		- [X] tags
+
+	- [X] Listagem de transactions
+		- [X] filtro por data, tag, forma, destino, tags, etc.
+		- [X] ordenação por data, valor, tag, etc.
+		- [ ] front-end
+
+	- [ ] Testar com extrato pessoa física
+
+	Meta de gastos por tag (planejamento)
+
+	Barra de progressão em % de gasto / planejado
+
+	Gráfico de pizza com % de gasto por tag
+		- Meta
+		- Real
+
+	Gráfico de linha com receitas por mês
+
+MVP
+	definir tags padrão: alimentação, transporte, saúde, lazer, educação, moradia, outros
+
+V2 (consultar anotacões físicas):
+	Extrato do cartão
+	- forma = crédito (adicionar)
+	- transactions where forma = crédito
+
+	Caixinhas:
+		- descr, valor meta, valor atual, valor depositado, prazo
+		- retorno, % atingida, aporte mensal necessário
+		- % tempo corrido vs % % atingida
+	Investimentos
